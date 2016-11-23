9cca006dedebdc52af7e81e023570c3633cf1adc
Sophia L <Sophia L>
2016-11-23 13:44:02 -0500

    Updated protocol: add live feed to serial comm

--- basic_protocol_slave/basic_protocol_slave.ino
+++ basic_protocol_slave/basic_protocol_slave.ino
@@ -2,7 +2,7 @@
 #define M_ID     0
 #define MY_ID    1
 #define FIN      10
-#define MSIZE    3
+#define MSIZE    3 //Next 3 bytes are the last
 
 void setup(){
  Serial.begin(57600);
@@ -14,7 +14,6 @@ void loop(){
 }
 
 
-
 int reseiveIntMessageSlvae(){
 
 int recover = 0;
@@ -23,32 +22,40 @@ int recover = 0;
 
    delay(5);
    uint8_t _t = Serial.read();
-
+//   Serial.println(_t);
+   
    if(_t == TILDE){
 
-     delay(5);
+//     delay(5);
      uint8_t _t = Serial.read();
+     //Serial.println(_t);
 
      if(_t == M_ID){
 
        delay(5);
        uint8_t _t = Serial.read();
+       //Serial.println(_t);
 
-       if(Serial.read() == MY_ID){
+       if(_t == MY_ID){
 
          delay(5);
          uint8_t _t = Serial.read();
+//         Serial.println(_t);
 
-         if(Serial.read() == MSIZE){
+         if(_t == MSIZE){
 
-          
+//           delay(5);          
            byte partOne;
            byte partTwo; 
            int counter = 0;
 
-           do{
+           do {
                partOne = Serial.read();
+               //Serial.print("High byte: ");
+               //Serial.println(partOne);
                partTwo = Serial.read();
+               //Serial.print("Low byte: ");
+               //Serial.println(partTwo);
                counter +=2;
                
                _t = Serial.read();
@@ -56,12 +63,14 @@ int recover = 0;
            while(counter <= MSIZE && _t != 10);
 
              recover = ((int)partOne)*256 + partTwo;
+             //Serial.print("Recover: ");
+             //Serial.println(recover);
 
-           char received = 'y';
+           //char received = 'y';
 
-           Serial.write(TILDE);
-           Serial.write(MY_ID);
-           Serial.write(received);
+           //Serial.write(TILDE);
+           //Serial.write(MY_ID);
+           //Serial.write(received);
          }
        }
      }
